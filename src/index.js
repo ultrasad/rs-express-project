d96a12ad9aba5f6c1e9de28969934b86059806c9
Hanajung <kissing-bear@hotmail.com>
2019-11-15 16:46:20 +0700

    Setup Project

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//console.log('express');
+
+const express = require('express');
+const swig = require('swig');
+const path = require('path');
+
+const customers = require('./customer.json');
+
+const app = express();
+
+app.set('view engine', 'swig');
+app.engine('swig', swig.renderFile);
+app.set('views', path.resolve(__dirname, 'views'));
+
+console.log("dirname:", __dirname);
+
+app.use(express.static('public')); //middleware to public path
+
+app.get('/', (req, res) => {
+    //res.send("<h1>Hello Express !!</h1>");
+    console.log('customers => ', customers);
+    //res.render('index.swig');
+    res.send('OK...');
+});
+
+app.get('/about', (req, res) => {
+    //res.send("<h1>About Express !!</h1>");
+    res.render('about.swig');
+});
+
+app.get('/customer', (req, res) => {
+    //res.send("<h1>About Express !!</h1>");
+    res.render('customers/index.swig', { customers: customers });
+});
+
+app.listen(3000);
